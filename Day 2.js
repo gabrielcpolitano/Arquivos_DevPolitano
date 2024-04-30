9a1fba5589e46368f14d6b0431f8fe5f6d33b4ba
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-04-30 16:18:09 -0300

    Obrigado Deus

---
+++ Day 2.js
@@ -0,0 +1,11 @@
+function numero(a){
+    let numero = a % 2;
+
+    if (numero == 0) {
+        console.log('É par');
+    } else {
+        console.log('É impar');
+    }
+}
+
+numero(10);
---
+++ Day 3.js
@@ -0,0 +1,11 @@
+function conta(a, b){
+    if (a == b) {
+        c = a + b;
+        console.log(c)
+    } else {
+        c = a * b;
+        console.log(c)
+    }
+}
+
+conta(5, 4)
