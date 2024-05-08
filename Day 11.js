da992f45d475974de4de01ab52919cf0b3e9d454
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-05-07 21:58:46 -0300

    Obrigado Deus

---
+++ Day 11.js
@@ -0,0 +1,12 @@
+function divisão(a, b){
+    var a
+    var b
+    
+    if ((a % b) === 0){
+        console.log('verdadeiro')
+    }else{
+        console.log('falso')
+    }
+}
+
+divisão(10, 3);
