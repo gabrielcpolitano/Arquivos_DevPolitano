a8ceb437953576568624d2a646b7b321e4ad397f
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-05-18 01:35:20 -0300

    Obrigado Deus

--- Dev1.js
+++ Dev1.js
@@ -1,6 +1,6 @@
 // Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
 
-let idade = 18;
-let peso = 70;
+let idade = 5;
+let peso = 35;
 
-console.log(idade + " " + peso);
+console.log(`Sua idade:${idade}  Seu peso:${peso}`);
