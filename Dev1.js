27581dd1cb555ace66df77092b741638b14534b5
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-05-21 18:30:46 -0300

    Obrigado Deus

--- Dev1.js
+++
@@ -1,6 +0,0 @@
-// Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
-
-let idade = 5;
-let peso = 35;
-
-console.log(`Sua idade:${idade}  Seu peso:${peso}`);
--- Dev6.js
+++
@@ -1,2 +0,0 @@
-// Declare duas variáveis e atribua valores booleanos a elas. Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. Imprima os resultados.
-
